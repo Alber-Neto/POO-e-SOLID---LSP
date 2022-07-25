54f375cbfe366617ab2edf719a8ba7db3e5dda1f
Alber-Neto <84081082+Alber-Neto@users.noreply.github.com>
2022-07-25 09:05:50 -0300

    Add files via upload

--- classes/src/br/uern/di/pa/abb/nobrega/antonio/Main.java
+++ classes/src/br/uern/di/pa/abb/nobrega/antonio/Main.java
@@ -1,46 +1,21 @@
 package br.uern.di.pa.abb.nobrega.antonio;
 
-import java.util.Scanner;
-import br.uern.di.pa.abb.nobrega.antonio.Papagaio;
-import br.uern.di.pa.abb.nobrega.antonio.Currupio;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		int distancia;
-		int opcao;
 		
-		Scanner input = new Scanner(System.in);
+	Papagaio papagaio=new Papagaio(10);
+	Currupio currupio=new Currupio(20);
+	
+	papagaio.cantar();
+	System.out.println("papagaio voa: " + papagaio.voar(10));
+	System.out.println("papagaio voa: " + papagaio.voar(20));
+	
+	currupio.cantar();
+	System.out.println("currupio voa: " + currupio.voar(5));
+	System.out.println("papagaio voa: " + currupio.voar(10));
 		
-		System.out.println("digite 1 para papagaio e 2 para currupio");
-		opcao=input.nextInt();
-		
-		if(opcao == 1) {
-			
-			System.out.println("digite uma distancia");
-			distancia=input.nextInt();
-			
-			Papagaio papagaio= new Papagaio(distancia);
-			papagaio.setDistancia_maxima(distancia);
-			
-			papagaio.cantar();
-			papagaio.voar(distancia);
-			
-		}
-		if(opcao == 2) {
-			
-			System.out.println("digite uma distancia");
-			distancia=input.nextInt();
-			
-			Currupio currupio= new Currupio(distancia);
-			currupio.setDistancia_maxima(distancia);
-			
-			currupio.cantar();
-			currupio.voar(distancia);
-			
-		}
-		
-
 	}
-
 }
